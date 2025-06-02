7a8f738d60304acbee8e51a78bc54c9587161311
aramfilak <aram.filak@outlook.de>
2025-06-02 12:56:17 +0200

    install tiptap rte-editor from https://tiptap.niazmorshed.dev/

---
+++ blog-nextjs/src/app/blogs/[blogId]/edit/page.tsx
@@ -0,0 +1,9 @@
+"use client";
+
+import TiptapEditor from "@/components/toolbars/tiptap-editor";
+import { useState } from "react";
+
+export default function BlogEdit() {
+  const [content, setContent] = useState("<h2>Hello world 🌍</h2>");
+  return <TiptapEditor content={content} onChange={setContent} />;
+}
