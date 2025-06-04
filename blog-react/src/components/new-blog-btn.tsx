7b59f694dd1eead54b837d9135016705413a35ae
aramfilak <aram.filak@outlook.de>
2025-06-04 23:44:23 +0200

    adjust min-height of add new blog card

--- blog-react/src/components/new-blog-btn.tsx
+++ blog-react/src/components/new-blog-btn.tsx
@@ -5,7 +5,7 @@ import { CirclePlusIcon } from "lucide-react";
 export function NewBlogBtn() {
   return (
     <NavLink to="/blog/new">
-      <Card className="h-full hover:cursor-pointer group flex flex-col items-center justify-center gap-4">
+      <Card className="h-full hover:cursor-pointer group flex flex-col items-center justify-center gap-4 min-h-[350px]">
         <CirclePlusIcon className="size-12 text-muted-foreground group-hover:text-primary transition-all duration-300 ease-in-out group-hover:scale-110" />
         <p className="text-muted-foreground group-hover:text-primary transition-colors duration-300 ease-in-out">
           Neuer Blogbeitrag
