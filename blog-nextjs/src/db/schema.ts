b0754b84c63391d1946182eaff49fa574016e4c9
aramfilak <aram.filak@outlook.de>
2025-06-04 10:08:57 +0200

    Refactor imageUrl field in blog schema to use an empty string as default instead of a placeholder URL, improving data consistency.

--- blog-nextjs/src/db/schema.ts
+++ blog-nextjs/src/db/schema.ts
@@ -8,18 +8,13 @@ import {
   integer,
 } from "drizzle-orm/pg-core";
 
-const defaultImageUrl =
-  "https://images.unsplash.com/photo-1488190211105-8b0e65b80b4e?q=80&w=3270&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D";
-
 export const blogs = pgTable("Blog", {
   id: serial("id").primaryKey(),
   author: varchar("author", { length: 255 }).notNull(),
   title: varchar("title", { length: 255 }).notNull(),
   description: text("description").notNull(),
   content: text("content").notNull(),
-  imageUrl: varchar("imageUrl", { length: 512 })
-    .notNull()
-    .default(defaultImageUrl),
+  imageUrl: varchar("imageUrl", { length: 512 }).notNull().default(""),
   tags: text("tags")
     .array()
     .notNull()
