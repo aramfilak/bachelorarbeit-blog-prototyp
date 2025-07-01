6abf07da9593fa6d67ce48a5b7d39e6b5a567e9a
aramfilak <aram.filak@outlook.de>
2025-07-01 13:45:11 +0200

    remove unused database seeding

--- blog-nextjs/src/db/index.ts
+++ blog-nextjs/src/db/index.ts
@@ -1,20 +1,5 @@
 import { neon } from "@neondatabase/serverless";
 import { drizzle } from "drizzle-orm/neon-http";
-// import { blogs } from "./schema";
-// import { blogSeeds } from "./seed";
 
 const sql = neon(process.env.DATABASE_URL!);
 export const db = drizzle({ client: sql });
-
-// async function main() {
-//   console.log("🌱 Seeding database...");
-
-//   try {
-//     await db.insert(blogs).values(blogSeeds);
-//     console.log("✅ Database seeded successfully!");
-//   } catch (error) {
-//     console.error("❌ Error seeding database:", error);
-//   }
-// }
-
-// main();
